665b76e65de9aebbfb1fcb653fe60f0c97c83903
deston <destonchung@gmail.com>
2023-07-25 23:54:10 +0800

    Add logic to use clear and number buttons

--- script.js
+++ script.js
@@ -31,12 +31,42 @@ function operate(operator, num1, num2) {
     return operatorMapping[operator](num1, num2);
 }   
 
-let clearButton = document.querySelector('.clear')
-let displayScreen = document.querySelector('.display')
-
-clearButton.addEventListener('click', clear);
-
 // function for clearing display
 function clear() {
     displayScreen.textContent = ''
+    pointUsed = false;
 }
+
+// function to update display 
+function updateDisplay() {
+    
+    console.log(pointUsed)
+
+    let buttonPressText = this.textContent;
+
+    if ((pointUsed) & (buttonPressText === '.')) {
+        return;
+    } else if ((displayText[displayText.length - 1] === '.') &
+               (buttonPressText === '.')) {
+        return;
+    }
+    else {
+        displayText += buttonPressText;
+        displayScreen.textContent = displayText;
+        if (buttonPressText === '.') {
+            pointUsed = true;
+        }
+    } 
+}
+
+let clearButton = document.querySelector('.clear')
+let displayScreen = document.querySelector('.display')
+let numberButtonList = document.querySelectorAll('.number')
+let displayText = '';
+let currVal;
+let pointUsed = false;
+
+clearButton.addEventListener('click', clear);
+numberButtonList.forEach( button =>
+    button.addEventListener('click', updateDisplay)
+);
