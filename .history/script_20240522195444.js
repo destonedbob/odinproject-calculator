e7ed6f8bc58bbbde0b1d1db55eb82ae537247c5d
Deston Chung <destonchung@gmail.com>
2024-05-22 19:56:13 +0800

    add logic to fix small decimal issue

--- .history/script_20240522195444.js
+++ .history/script_20240522195444.js
@@ -10,17 +10,17 @@ function add(num1, num2) {
 
 // subtract
 function subtract(num1, num2) {
-    return roundToDecimalPlaces(num1 - num2, 10);
+    return num1 - num2;
 }
 
 // multiply
 function multiply(num1, num2) {
-    return roundToDecimalPlaces(num1 * num2, 10);
+    return num1 * num2;
 }
 
 // divide
 function divide(num1, num2) {
-    return roundToDecimalPlaces(num1 / num2, 10);
+    return num1 / num2;
 }
 
 function hasMoreThan10Decimal(num) {
